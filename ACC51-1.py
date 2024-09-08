56b08b542936b39c5404c7c5ad3f65a8f44a2b53
Kabi-2005 <144709450+Kabi-2005@users.noreply.github.com>
2024-09-08 18:08:41 +0530

    Update ACC51-1.py

--- ACC51-1.py
+++ ACC51-1.py
@@ -1,1 +1,16 @@
+arr = [1, 2, 3, 2, 1, 4]
+N = 2
+xor2 = 0
+for num in arr:
+    xor2 ^= num
+right = xor2 & -xor2
+num1 = 0
+num2 = 0
+for num in arr:
+    if num & right:
+        num1 ^= num
+    else:
+        num2 ^= num
+result = sorted([num1, num2])
+print(result)
 
