c41b66133cc9b4e3220e63526b92faad14b9e164
Kabi-2005 <144709450+Kabi-2005@users.noreply.github.com>
2024-09-08 18:20:39 +0530

    Update ACC51-2.py

--- ACC51-2.py
+++ ACC51-2.py
@@ -1,1 +1,11 @@
-
+n = int(input("Enter the number of words in the dictionary: "))
+dictionary = set(input("Enter the dictionary words separated by space: ").split())
+s = input("Enter the string: ")
+dp = [False] * (len(s) + 1)
+dp[0] = True
+for i in range(1, len(s) + 1):
+    for j in range(i):
+        if dp[j] and s[j:i] in dictionary:
+            dp[i] = True
+            break
+print(1 if dp[len(s)] else 0)
