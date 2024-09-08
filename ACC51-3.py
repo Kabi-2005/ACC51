50c63d0c28af6c0082e92b634ba5db1792f4fbc4
Kabi-2005 <144709450+Kabi-2005@users.noreply.github.com>
2024-09-08 18:25:40 +0530

    Update ACC51-3.py

--- ACC51-3.py
+++ ACC51-3.py
@@ -1,1 +1,25 @@
 
+from collections import deque
+n, m = map(int, input("Enter the range (N M): ").split())
+result = []
+for i in range(10):
+    queue = deque([i])
+    while queue:
+        stepnum = queue.popleft()
+        if stepnum > m:
+            continue
+        if stepnum >= n and stepnum <= m:
+            result.append(stepnum)
+        lastdigit = stepnum % 10
+        if stepnum == 0 or stepnum > m:
+            continue
+        if lastdigit > 0:
+            nextstepnum = stepnum * 10 + (last_digit - 1)
+            if nextstepnum <= m:
+                queue.append(nextstepnum)
+        if lastdigit < 9:
+            nextstepnum = stepnum * 10 + (lastdigit + 1)
+            if nextstepnum <= m:
+                queue.append(nextstepnum)
+result = sorted(result)
+print(" ".join(map(str, result)))
