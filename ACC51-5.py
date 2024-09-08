828083d4d429961bf5250f0f6acb1f0bb7c257b3
Kabi-2005 <144709450+Kabi-2005@users.noreply.github.com>
2024-09-08 18:30:49 +0530

    Update ACC51-5.py

--- ACC51-5.py
+++ ACC51-5.py
@@ -1,1 +1,14 @@
+n = int(input().strip())
+prices = [int(input().strip()) for _ in range(n)]
+money = int(input().strip())
+start = 0
+currentsum = 0
+maxlength = 0
+for end in range(n):
+    currentsum += prices[end]
+    while currentsum > money:
+        currentsum -= prices[start]
+        start += 1    
+    maxlength = max(maxlength, end - start + 1)
+print(maxlength)
 
